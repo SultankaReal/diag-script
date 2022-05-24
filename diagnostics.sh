1f89d4fa361307d4f40ba6e9f513d5cf49985216
Nursultan <77805226+SultankaReal@users.noreply.github.com>
2022-05-24 16:27:14 +0300

    Update diagnostics.sh

--- diagnostics.sh
+++ diagnostics.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# Скрипт по сбору диагностики с кластера k8s
+# Скрипт по сбору диагностики с кластера k8s. На выходе имеем zip-архив k8s.zip.
 # Файлы: log.txt, events.txt, describe_nodes.txt, pvc_pv.txt, operations.txt
 
 FILE_LOG=log.txt
@@ -16,56 +16,58 @@ do
 touch $file
 done
 
-
 # Pods 
 date >> $FILE_LOG
-echo "-----------------------------------" >> $FILE_LOG
+echo "-----------------------------------" &>> $FILE_LOG
 echo "Введите cluster-id k8s кластера: "
 read cluster_id
-# if wc -c new.txt | awk '{print $1}' == 21 
+# echo $cluster_id > new.txt
+# number = (wc -c new.txt | awk '{print $1}')
+# echo $number
+# if [ $number -eq 21 ];
 # then
-echo "-----------------------------------" >> $FILE_LOG
-yc managed-kubernetes cluster get --id=$cluster_id >> $FILE_LOG
-echo "-----------------------------------" >> $FILE_LOG
-yc managed-kubernetes cluster get-credentials --id $cluster_id --external --force >> $FILE_LOG
+echo "Описание кластера: " &>> $FILE_LOG
+yc managed-kubernetes cluster get --id=$cluster_id &>> $FILE_LOG
+echo "Подключаемся к кластеру k8s: " &>> $FILE_LOG
+yc managed-kubernetes cluster get-credentials --id $cluster_id --external --force &>> $FILE_LOG
 echo "Введите тип проблемного ресурса (пример: deployment, daemonset, statefulset и так далее): "
 read resource_type
 echo "Введите имя ресурса (имя deployment-a, daemonset-a, statefulset-a и так далее): "
 read resource_name
 echo "Введите имя namespace-a, где находится проблемный ресурс: "
 read namespace
-echo "-----------------------------------" >> $FILE_LOG
-kubectl get $resource_type $resource_name -n $namespace >> $FILE_LOG
-echo "-----------------------------------" >> $FILE_LOG
-kubectl get pods -n $namespace | grep $resource_name | awk '{print $1}' > list.txt
+echo "-----------------------------------" &>> $FILE_LOG
+kubectl get $resource_type $resource_name -n $namespace &>> $FILE_LOG
+echo "-----------------------------------" &>> $FILE_LOG
+kubectl get pods -n $namespace | grep $resource_name | awk '{print $1}' &> list.txt
 while read pod
 do
-echo "----------------------------- ------" >> $FILE_LOG
-echo "Лог пода: "  >> $FILE_LOG
-kubectl logs -n $namespace $pod >> $FILE_LOG
-echo "----------------------------- ------" >> $FILE_LOG
-echo "Дескрайб пода: " >> $FILE_LOG
-kubectl describe pods -n $namespace $pod >> $FILE_LOG
+echo "-----------------------------------" &>> $FILE_LOG
+echo "Лог пода: "  &>> $FILE_LOG
+kubectl logs -n $namespace $pod &>> $FILE_LOG
+echo "Дескрайб пода: " &>> $FILE_LOG
+kubectl describe pods -n $namespace $pod &>> $FILE_LOG
 done < list.txt
 rm list.txt
-echo "-----------------------------------" >> $FILE_LOG
+# else
+# break
 # fi
 
 # Events
 date >> $FILE_EVENTS
-echo "-----------------------------------" >> $FILE_EVENTS
-kubectl get events --all-namespaces  --sort-by='.metadata.creationTimestamp' >> $FILE_EVENTS
+echo "Ивенты с сортировкой по timestamp-ам: " &>> $FILE_EVENTS
+kubectl get events --all-namespaces  --sort-by='.metadata.creationTimestamp' &>> $FILE_EVENTS
 
 # Nodes
 date >> $FILE_NODES
-echo "-----------------------------------" >> $FILE_NODES
-yc managed-kubernetes cluster list-node-groups --id=$cluster_id >> $FILE_NODES
-echo "-----------------------------------" >> $FILE_NODES
-yc managed-kubernetes cluster list-nodes --id=$cluster_id >> $FILE_NODES
-echo "-----------------------------------" >> $FILE_NODES
-kubectl get nodes -o wide >> $FILE_NODES
-echo "-----------------------------------" >> $FILE_NODES
-kubectl describe nodes $(kubectl get pods -o wide | awk '{print $7}') >> $FILE_NODES
+echo "Листинг нод-групп: " &>> $FILE_NODES
+yc managed-kubernetes cluster list-node-groups --id=$cluster_id &>> $FILE_NODES
+echo "Листинг нод: " &>> $FILE_NODES
+yc managed-kubernetes cluster list-nodes --id=$cluster_id &>> $FILE_NODES
+echo "Список нод в кластере: " &>> $FILE_NODES
+kubectl get nodes -o wide &>> $FILE_NODES
+echo "Дескрайб нод: " &>> $FILE_NODES
+kubectl describe nodes $(kubectl get pods -o wide | awk '{print $7}') &>> $FILE_NODES
 
 # PVC/PV/StorageClass
 echo "Есть проблема с PV/PVC? (если да, введи true; если нет, введи false)"
@@ -78,19 +80,17 @@ read pvc_name
 echo "Введите имя namespace-a pvc: "
 read pvc_namespace
 date >> $FILE_PVC_PV
-echo "-----------------------------------" >> $FILE_PVC_PV
-kubectl get pvc $pvc_name -n $pvc_namespace >> $FILE_PVC_PV
-echo "-----------------------------------" >> $FILE_PVC_PV
-kubectl get pvc $pvc_name -n $pvc_namespace -o yaml >> $FILE_PVC_PV
+echo "-----------------------------------" &>> $FILE_PVC_PV
+kubectl get pvc $pvc_name -n $pvc_namespace &>> $FILE_PVC_PV
+echo "-----------------------------------" &>> $FILE_PVC_PV
+kubectl get pvc $pvc_name -n $pvc_namespace -o yaml &>> $FILE_PVC_PV
 rm True
 fi
 
-
 # Operations
 date >> $FILE_OPERATIONS
-echo "-----------------------------------" >> $FILE_OPERATIONS
-yc managed-kubernetes cluster list-operations --id=$cluster_id >> $FILE_OPERATIONS
-
+echo "Листинг операций в кластера: " &>> $FILE_OPERATIONS
+yc managed-kubernetes cluster list-operations --id=$cluster_id &>> $FILE_OPERATIONS
 
 # Creating zip-archive and deleting old files
 for file in $files
